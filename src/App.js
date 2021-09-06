97beb8bb156c6833236c1b2777c5f6aa86ce72a6
LuisHans <jluishans@outlook.com>
2021-09-06 14:43:28 -0500

    Building & Using a Custom Context Provider Component

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     <React.Fragment>
       <MainHeader />
       <main>
-        {!ctx && <Login />}
-        {ctx && <Home />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
     </React.Fragment>
   );
